fea0292a2e0a0bbf7844eabc9b656bf114047409
Ayan Dhiman <dhimanayan.ad@gmail.com>
2024-05-06 19:12:52 +0530

    Disabled unnecessary test method

--- src/test/java/webapp/restapi/JWTToken/JwtTokenApplicationTests.java
+++ src/test/java/webapp/restapi/JWTToken/JwtTokenApplicationTests.java
@@ -1,11 +1,13 @@
 package webapp.restapi.JWTToken;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JwtTokenApplicationTests {
-
+	
+	@Disabled
 	@Test
 	void contextLoads() {
 	}
